5cad5d2ebe4c1c48a80e4e0d9ca07ebc8375374c
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-01-10 17:13:37 +0100

    Conexión BBDD y comprobación de login funcional, falta decidir cómo aplicar la paleta al tema + cambio de nombre al fichero discober.php por discover.php

--- login.php
+++ login.php
@@ -1,3 +1,70 @@
+<?php
+session_start();
+if (isset($_POST["submit"])) {
+    if (!empty($_POST["email"]) && !empty($_POST["password"])) {
+        try {
+            $hostname = "localhost";
+            $dbname = "tinder";
+            $username = "admin";
+            $pw = "admin123";
+            $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", "$username", "$pw");
+        } catch (PDOException $e) {
+            echo "Error connectant a la BD: " . $e->getMessage() . "<br>\n";
+            exit;
+        }
+        try {
+            //preparar consulta y sanear los parámetros
+            $query = $pdo->prepare("SELECT * FROM users WHERE mail = :mail AND password = :password");
+            $query->bindParam(':mail', $_POST["email"]);
+            $query->bindParam(':password', hash('sha512', $_POST["password"]));
+            $query->execute();
+        } catch (PDOException $e) {
+            echo "Error de SQL<br>\n";
+            //comprobacion de errores
+            $e = $query->errorInfo();
+            if ($e[0] != '00000') {
+                echo "\nPDO::errorInfo():\n";
+                die("Error accedint a dades: " . $e[2]);
+            }
+        }
+        //si login correcto, ir a vista discover
+        if ($query->rowCount() > 0) {
+            $row = $query->fetch();
+            echo $row["mail"];
+            echo $row["password"];
+            //guardar sesión en cookie
+            setcookie("login_cookie", $row["mail"], time() + 3600);
+            //redirigir a discover
+            header("Location: discover.php");
+
+            //si el login no es correcto buscamos si existe el usuario para mostrar password incorrecto     
+        } else {
+            try {
+                //preparar consulta y sanear los parámetros
+                $query = $pdo->prepare("SELECT * FROM users WHERE mail = :mail");
+                $query->bindParam(':mail', $_POST["email"]);
+                $query->execute();
+            } catch (PDOException $e) {
+                echo "Error de SQL<br>\n";
+                //comprobacion de errores
+                $e = $query->errorInfo();
+                if ($e[0] != '00000') {
+                    echo "\nPDO::errorInfo():\n";
+                    die("Error accedint a dades: " . $e[2]);
+                }
+            }
+            if ($query->rowCount() > 0) {
+                $message = "<div class='alert alert-danger'>Password incorrecto</div>";
+            } else {
+                $message = "<div class='alert alert-danger'>Email y password incorrectos</div>";              
+            }
+        }
+    } else {
+        $message = "<div class='alert alert-danger'>Se requiere llenar ambos campos</div>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -12,7 +79,7 @@
 </head>
 
 <body id="login">
-    <div class="container">
+    <div class="flex-container">
         <main>
             <div id="logo">
                 <h1 class="funnel-display"><a href="index.php">LOGO PLACEHOLDER</a></h1>
@@ -24,7 +91,8 @@
                     <input type="email" name="email" placeholder="usuario@ieti.site">
                     <label for="password" class="funnel-display">Contraseña</label>
                     <input type="password" name="password" placeholder="******">
-                    <input type="submit" value="Iniciar Sesión">
+                    <?php echo $message; ?>
+                    <input type="submit" name="submit" value="Iniciar Sesión">
                 </form>
             </div>
         </main>
