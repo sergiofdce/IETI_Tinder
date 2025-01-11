f34cabe097874250a57579070b9d3d53cd8fd078
Sergio fdce <sergio.fdce@gmail.com>
2025-01-11 14:20:18 +0100

    Maquetar dimensiones de la app

---
+++ discober.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+
+      <header>
+            <h1>Discober</h1>
+      </header>
+
+      <main>
+            <div class="container">
+                  
+            </div>
+            
+      </main>
+
+      <footer>
+            <nav>
+                  <ul>
+                        <li><a href="discober.php">Descobrir</a></li>
+                        <li><a href="messages.php">Missatges</a></li>
+                        <li><a href="profile.php">Perfil</a></li>
+                  </ul>
+            </nav>
+      </footer>
+
+</body>
+
+</html>
