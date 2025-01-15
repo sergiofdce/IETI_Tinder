525d2c97f4883a1eafb49c1d03a94fe48633acd1
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-01-15 18:08:00 +0100

    cambios conexión bbdd

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include 'includes/functions.php';
 //log de logout
 logEvent("logout", "El usuario " . $_SESSION["email"] . " ha cerrado sesion", $_SESSION["email"]);
 //cerrar sesion
