ef8dde28bde70cd862d038381b3af9802687a4ce
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-01-16 04:26:34 +0100

    descomentar logevent logout

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
       session_start();
       include 'includes/functions.php';
       //log de logout
-      // logEvent("logout", "El usuario " . $_SESSION["email"] . " ha cerrado sesion", $_SESSION["email"]);
+      logEvent("logout", "El usuario " . $_SESSION["email"] . " ha cerrado sesion", $_SESSION["email"]);
       //cerrar sesion
       unset($_SESSION["user"]);
       unset($_SESSION["email"]);
