4401db01bab58fc16e600bcea4a93d1f907b6a89
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-01-22 18:59:25 +0100

    algoritmo implementado

--- discover.php
+++ discover.php
@@ -67,73 +67,75 @@ logEvent("page_view", "El usuario ha accedido a la página Discover", $_SESSION[
 
             $query = "
                   SELECT DISTINCT u.id, u.name, u.surname, u.alias, u.birth_date, u.location, u.genre, u.sexual_preference, u.email,
-                  GROUP_CONCAT(ui.path ORDER BY ui.id ASC) AS images,
-                  -- Calcular la distancia entre el usuario y el usuario logueado usando Haversine
-                  (
-                        6371 * acos(
-                              cos(radians(:lat)) * cos(radians(SUBSTRING_INDEX(u.location, ',', 1))) *
-                              cos(radians(SUBSTRING_INDEX(u.location, ',', -1)) - radians(:long)) +
-                              sin(radians(:lat)) * sin(radians(SUBSTRING_INDEX(u.location, ',', 1)))
-                        )
-                  ) AS distance
+                        GROUP_CONCAT(ui.path ORDER BY ui.id ASC) AS images,
+                        -- Calcular la distancia entre el usuario y el usuario logueado usando Haversine
+                        (
+                              6371 * acos(
+                                    cos(radians(:lat)) * cos(radians(SUBSTRING_INDEX(u.location, ',', 1))) *
+                                    cos(radians(SUBSTRING_INDEX(u.location, ',', -1)) - radians(:long)) +
+                                    sin(radians(:lat)) * sin(radians(SUBSTRING_INDEX(u.location, ',', 1)))
+                              )
+                        ) AS distance,
+                        -- Calcular el score basado en la interacción
+                        (SELECT COUNT(*) 
+                        FROM matches m 
+                        WHERE m.sender_id = u.id AND m.status = 'accepted') -
+                        (SELECT COUNT(*) 
+                        FROM matches m 
+                        WHERE m.sender_id = u.id AND m.status = 'rejected') AS interaction_score
                   FROM users u
                   LEFT JOIN user_images ui ON u.id = ui.user_id
                   WHERE u.id != :session_id
-                  -- Seleccionar usuarios con preferencias sexuales compatibles
-                    AND (
+                  AND (
                         (:user_genre = 'home' AND :user_preference = 'heterosexual' AND u.genre = 'dona')
                         OR (:user_genre = 'home' AND :user_preference = 'homosexual' AND u.genre = 'home')
                         OR (:user_genre = 'dona' AND :user_preference = 'heterosexual' AND u.genre = 'home')
                         OR (:user_genre = 'dona' AND :user_preference = 'homosexual' AND u.genre = 'dona')
                         OR (:user_preference = 'bisexual')
                         OR (:user_genre = 'no binari')
+                  )
+                  AND NOT EXISTS (
+                        SELECT 1 
+                        FROM matches m_accepted
+                        WHERE m_accepted.status = 'accepted'
+                        AND (
+                              (m_accepted.sender_id = :session_id AND m_accepted.receiver_id = u.id)
+                              OR 
+                              (m_accepted.sender_id = u.id AND m_accepted.receiver_id = :session_id)
                         )
-                  -- Exclude users that have an accepted match with session_id (check both directions)
-                  AND NOT EXISTS (
-                  SELECT 1 
-                  FROM matches m_accepted
-                  WHERE m_accepted.status = 'accepted'
-                  AND (
-                        (m_accepted.sender_id = :session_id AND m_accepted.receiver_id = u.id)
-                        OR 
-                        (m_accepted.sender_id = u.id AND m_accepted.receiver_id = :session_id)
                   )
-                  )
-                  -- Exclude users based on rejected matches, but only if session_id has interacted with them
                   AND NOT EXISTS (
-                  SELECT 1 
-                  FROM matches m_rejected
-                  WHERE m_rejected.status = 'rejected'
+                        SELECT 1 
+                        FROM matches m_rejected
+                        WHERE m_rejected.status = 'rejected'
+                        AND (
+                              (m_rejected.sender_id = :session_id AND m_rejected.receiver_id = u.id)
+                              OR 
+                              (m_rejected.sender_id = u.id AND m_rejected.receiver_id = :session_id AND NOT EXISTS (
+                                    SELECT 1 
+                                    FROM matches m_pending
+                                    WHERE m_pending.sender_id = :session_id
+                                    AND m_pending.receiver_id = u.id
+                              ))
+                        )
+                  )
                   AND (
-                        (m_rejected.sender_id = :session_id AND m_rejected.receiver_id = u.id)
-                        OR 
-                        (m_rejected.sender_id = u.id AND m_rejected.receiver_id = :session_id AND NOT EXISTS (
+                        NOT EXISTS (
                               SELECT 1 
-                              FROM matches m_pending
-                              WHERE m_pending.sender_id = :session_id
-                              AND m_pending.receiver_id = u.id
-                        ))
-                  )
-                  )
-                  AND (
-                  -- Include users that haven't had any interaction with session_id
-                  NOT EXISTS (
-                        SELECT 1 
-                        FROM matches m1
-                        WHERE m1.sender_id = :session_id 
-                        AND m1.receiver_id = u.id
-                  )
-                  -- OR include users that have given a pending like to session_id
-                  OR EXISTS (
-                        SELECT 1 
-                        FROM matches m2
-                        WHERE m2.sender_id = u.id 
-                        AND m2.receiver_id = :session_id
-                        AND m2.status = 'pending'
-                  )
+                              FROM matches m1
+                              WHERE m1.sender_id = :session_id 
+                              AND m1.receiver_id = u.id
+                        )
+                        OR EXISTS (
+                              SELECT 1 
+                              FROM matches m2
+                              WHERE m2.sender_id = u.id 
+                              AND m2.receiver_id = :session_id
+                              AND m2.status = 'pending'
+                        )
                   )
                   GROUP BY u.id, u.name, u.surname, u.alias, u.birth_date, u.location, u.genre, u.sexual_preference, u.email
-                  ORDER BY distance ASC;
+                  ORDER BY ((interaction_score * 0.7) + (1 - (distance / MAX(distance)) * 0.3)) DESC;
                   ";
 
 
