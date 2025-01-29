7bffc2d50183391b0964ccd8d03d605e6d5417cc
Sergio fdce <sergio.fdce@gmail.com>
2025-01-29 22:08:52 +0100

    Fixes

--- admin/index.php
+++ admin/index.php
@@ -3,13 +3,14 @@ session_start();
 include '../includes/functions.php';
 require_once '../config/db_connection.php';
 
-logEvent("page_view", "El usuario ha accedido a la página Admin", $_SESSION["email"]);
 
 if (!isset($_SESSION["user_id"])) {
     header("HTTP/1.0 403 Forbidden");
+    include('../errors/error403.php');
     die();
 }
 $user_id = $_SESSION["user_id"];
+logEvent("page_view", "El usuario ha accedido a la página Admin", $_SESSION["email"]);
 $query = "SELECT privileges FROM users WHERE id = :user_id";
 $params = [':user_id' => $user_id];
 $user = executeQuery($pdo, $query, $params);
